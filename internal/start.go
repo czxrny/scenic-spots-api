bd98f56ca8032c0f3d8a51f8b5807718a93a19d7
Szymon Czarnecki <sczarnecki091@gmail.com>
2025-06-07 17:42:51 +0200

    feat: added CORS middleware allowing requests from port 5173 on local machine

--- internal/start.go
+++ internal/start.go
@@ -45,7 +45,25 @@ func initializeHandlers() {
 func startTheServer() error {
 	port := os.Getenv("PORT")
 	logger.Info("Starting server on port " + port)
-	err := http.ListenAndServe(":"+port, nil)
+
+	corsMiddleware := func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+			if r.Method == "OPTIONS" {
+				w.WriteHeader(http.StatusOK)
+				return
+			}
+
+			next.ServeHTTP(w, r)
+		})
+	}
+
+	handlerWithCORS := corsMiddleware(http.DefaultServeMux)
+
+	err := http.ListenAndServe(":"+port, handlerWithCORS)
 	if err != nil {
 		logger.Error(err.Error())
 		return err
